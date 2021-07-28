e559c36f42661ef1561743e40a74465969bb07df
jbyun <binifia@gmail.com>
2021-07-28 18:57:54 +0900

    daumTest.php updated

--- app/src/main/assets/daumTest.php
+++ app/src/main/assets/daumTest.php
@@ -11,7 +11,7 @@ header("Content-Type: text/html; charset=UTF-8");
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
             // 예제를 참고하여 다양한 활용법을 확인해 보세요.
 
-            setAddress(data.zonecode, data.roadAddress, jibunAddress);
+            Android.setAddress(data.zonecode, data.roadAddress, jibunAddress);
             // 현재 상황 : DaumPostalCD.kt 에서 javascriptinterface를 사용 못함 -> setAddress() 호출 불가능
         }
     }).open();
