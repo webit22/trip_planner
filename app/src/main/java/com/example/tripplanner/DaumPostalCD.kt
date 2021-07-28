ee7cf2184ffcf907ab825ebd10b6139234bd4b81
jbyun <binifia@gmail.com>
2021-07-28 16:33:00 +0900

    POSTCODE UPDATED

--- app/src/main/java/com/example/tripplanner/DaumPostalCD.kt
+++ app/src/main/java/com/example/tripplanner/DaumPostalCD.kt
@@ -1,16 +1,14 @@
 package com.example.tripplanner
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
-import android.content.Context
+import android.content.ContentValues.TAG
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.webkit.*
-import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.example.tripplanner.databinding.ActivityDaumPostalCdBinding
 
 /* WebView - 다음 주소 검색 페이지 */
@@ -43,48 +41,44 @@ class DaumPostalCD : AppCompatActivity(){
             webViewClient.onPageFinished(this, "javascript:execDaumPostcode()")
 
             //javascript에서 인터페이스 호출 시 이름 "Android" 사용
-            addJavascriptInterface(AndroidBridge(), "Android")
+            addJavascriptInterface(AndroidBridge(this), "Android")
             loadUrl("http://localhost/android_asset/daum.html") //local web server > 주소검색 파일
         }
 
     }
 
-    private fun AndroidBridge(){
-        Log.d(TAG, "DaumPostalCD - AndroidBridge() called")
+    // proguard rules 바꿈
+    public class AndroidBridge(mContext: WebView){
         // 주소 세팅하는 메서드
-        // 자꾸 setAddress()를 호출하지 못함
+        // 자꾸 setAddress()를 호출하지 못함 - proguard rules 수정 필요함
 
         @JavascriptInterface
-        fun setExtraAddress(para: String){
-            binding.textJsData.text = para
-        }
-
-        @JavascriptInterface
-        fun setAddress(arg: String){
+        fun setAddress(arg1: String, arg2: String, arg3: String){
             // 파라미터 : data.zonecode, data.roadAddress, data.buildingName
             // handler를 통해 javascript 이벤트 반응
             val extra : Bundle? = null
-            val intent : Intent? = null
+            var intent : Intent? = null
 
-            extra?.putString("data", arg)
-            if(extra != null) intent?.putExtras(extra)
-
-            setResult(RESULT_OK, intent)
-            finish()
-
-            /*
-            val handler = Handler()
             val result = "($arg1);$arg2;$arg3"
-            Log.d(TAG, "DaumPostalCD - setAddress.result = $result")
+            Log.e(TAG, "DaumPostalCD - setAddress.result = $result")
+//            handler.post {
+//                extra?.putString("data", result)
+//                if (extra != null) intent?.putExtras(extra)
+//                startActivity(this, intent, extra)
+//
+//                setResult(RESULT_OK, intent)
+//                finish()
+//
+//                intent = Intent(mContext, SearchAddress::class.java)
+//                intent.putExtra("strAddr", result)
+//                startActivity(intent)
+//                finish()
+//            }
 
-            handler.post({
-                intent = Intent(this, SearchAddress::class.java)
-                intent.putExtra("strAddr", result)
-                startActivity(intent)
-                finish()
-            })
-            */
         }
+    }
+    companion object{
+        val handler = Handler()
     }
 
     override fun onBackPressed() {
